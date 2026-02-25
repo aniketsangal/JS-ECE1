ef8384ceb2671b5f00a67b00eb3c23db723880ff
XYZ <XYZ@example.cogit config --global user.email>
2026-02-25 10:20:29 +0530

    New folder

--- hello world/script.js
+++ hello world/script.js
@@ -13,3 +13,15 @@ console.log(typeof(x))
 num = null;
 console.log(num);
 console.log(typeof(num));
+
+
+const sym1 = Symbol(4);
+console.log(sym1);
+const sym2 = Symbol(4);
+console.log(sym2);
+if(sym1==sym2){
+    console.log("true")
+}
+else{
+    console.log("false")
+}
