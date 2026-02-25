c8e87e3e084f19ae55efed401fb17d64137f6ead
XYZ <XYZ@example.cogit config --global user.email>
2026-02-25 09:48:55 +0530

    first update

--- hello world/script.js
+++ hello world/script.js
@@ -1,4 +1,15 @@
-console.log("hello, world!")
-console.log("hello, world!")
-console.log("hello, world!")
-console.log("hello, world!")
+var num = 10;
+console.log(num);
+console.log(typeof(num));
+num = false;
+console.log(num);
+console.log(typeof(num));
+num = "hello";
+console.log(num);
+console.log(typeof(num))
+let x;
+console.log(x);
+console.log(typeof(x))
+num = null;
+console.log(num);
+console.log(typeof(num));
