5bf7e777a342da8f6d9059251be7785c47236501
XYZ <XYZ@example.cogit config --global user.email>
2026-03-11 09:44:35 +0530

    updated file

---
+++ loops/script.js
@@ -0,0 +1,37 @@
+/*
+let count =0;
+document.write("starting loop"+"<br>");
+for(count =0;count<10;count++){
+    document.write("current count:" + count);
+        document.write("<br>")
+}
+console.log("loop started");  
+
+
+const person = {
+    fname: "Ajay",
+    Inmae: "Singh",
+    Age:25
+};
+for (let x in person) {
+    console.log("person details " + x + ": " + person[x])
+// } 
+ var age = 19;
+ if (age>18){
+    document.write("<b>You are eligible to vote</b>")
+ }
+ else {
+    document.write("<b>You are a minor</b>")
+ }
+
+var grade = 'A';
+Case 'A': document.write("Good job</b>")
+var grade = 'B';
+Case 'B': document.write("Pretty Good</b>")
+var grade = 'C';
+Case 'C': document.write("Passed</b>")
+var grade = 'D';
+Case 'D': document.write("Not so good</b>")
+var grade = 'F';
+Case 'F': document.write("Failed</b>")
+*/
