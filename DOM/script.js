f9c8be1fc7306a5a5aacd78f29f587a9f18b7ca9
XYZ <XYZ@example.cogit config --global user.email>
2026-03-18 10:27:03 +0530

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,21 @@
+// const result = document.getElementById("as");
+// console.log(result);
+// result.innerHTML = "<b>Hello World !!</b>";
+
+// const result = document.getElementsByClassName("p1");
+// console.log(result);
+
+// const result = document.getElementById("as").getAttribute("class");
+
+// document.getElementById("p1").innerHTML = result;
+
+
+function OnMouseClick(){
+
+}
+function OnMouseOver(){
+
+}
+function  OnMouseOut(){
+    
+}
